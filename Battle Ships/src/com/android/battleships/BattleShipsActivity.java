0ba62a256a6fa6730bcee0ba35db42df9aef752a
Robert O'Dell <rodell@thegamerdex.com>
2011-09-28 14:09:25 -0500

    Initial Project Creation
    Signed-off-by: Robert O'Dell <rodell@thegamerdex.com>

---
+++ Battle Ships/src/com/android/battleships/BattleShipsActivity.java
@@ -0,0 +1,13 @@
+package com.android.battleships;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BattleShipsActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
